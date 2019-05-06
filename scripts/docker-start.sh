d39e3f92342582e3405e638535da62efcdafde97
Jacob Heater <jacobheater@gmail.com>
2019-05-06 17:19:01 -0400

    Updating startup script.

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -2,9 +2,15 @@
 
 echo "Starting Address Analyzer and Background Services\n"
 
+echo "Starting GeoIP"
 dotnet ./publish/AddressAnalyzer.GeoIp.Api.dll &
+echo "Starting Ping"
 dotnet ./publish/AddressAnalyzer.Ping.Api.dll &
+echo "Starting RDAP"
 dotnet ./publish/AddressAnalyzer.Rdap.Api.dll &
+echo "Starting Reverse DNS"
 dotnet ./publish/AddressAnalyzer.ReverseDns.Api.dll &
+echo "Starting Virus Total"
 dotnet ./publish/AddressAnalyzer.VirusTotal.Api.dll &
+echo "Starting Address Analyzer"
 dotnet ./publish/AddressAnalyzer.Api.dll
